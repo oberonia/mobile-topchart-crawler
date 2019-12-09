5e8613f807b9fb7f5b095e35e3ee420de9ae9b93
oberonia <over0n91@gmail.com>
2019-12-10 04:28:06 +0900

    Copy app size and export it to csv
    용량 정보를 복사해서 csv에 저장하도록 함

--- googleplay.py
+++ googleplay.py
@@ -21,6 +21,11 @@ next(reader)
 next(reader)
 next(reader)  # to skip header
 
+resultfile = open(r'E:\mobile-topchart-crawler\uk-result.csv', 'w', encoding='utf-8')
+storename = ['Game Name', 'Google Play']
+writer = csv.DictWriter(resultfile, fieldnames=storename)
+writer.writeheader()
+
 # 검색창에 검색
 test_count = 0
 
@@ -36,19 +41,23 @@ for line in reader:
     company_xpath = '//*[@id="fcxH9b"]/div[4]/c-wiz[2]/div/div[2]/div/c-wiz/c-wiz/c-wiz/div/div[2]/div/c-wiz/div/div/div[2]/div/div/div[1]/div/div/div[2]/a/div'
     company = driver.find_elements_by_xpath(company_xpath)
     for item in company:
-        if line[2] in item.text:
+        if line[2].lower() in item.text.lower():
             print(item.text)
-            # 게임명 //*[@id="fcxH9b"]/div[4]/c-wiz[2]/div/div[2]/div/c-wiz/c-wiz/c-wiz/div/div[2]/div[1]/c-wiz/div/div/div[2]/div/div/div[1]/div/div/div[1]/a/div
             result = item.find_element_by_xpath(
                 company_xpath+'/ancestor::div[@class="kCSSQe"]/div[1]/a/div')
             result.click()
 
             # 용량 정보 복사
-            # app_size_xpath = '//*[@id="fcxH9b"]/div[4]/c-wiz[3]/div/div[2]/div/div[1]/div/c-wiz[3]/div[1]/div[2]/div/div[2]/span/div/span'
-            # app_size = driver.find_element_by_xpath(app_size_xpath)
-            # # app_size = driver.find_element_by_xpath(
-            # #     '//*[@id="fcxH9b"]/descendant::span[@class="htlgb"]')
-            # print(app_size.text)
-            break
+            app_size_xpath = '(.//*[normalize-space(text()) and normalize-space(.)="Size"])[1]/following::div[1]'
+            WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, app_size_xpath)))
+            app_size = driver.find_element_by_xpath(app_size_xpath).text
+            print(app_size)
+        else:
+            print('Can not find that game: '+line[1]+' or developers: '+line[2])
+            app_size = 'N/A' # 완전 일치 게임을 못찾았으면 N/A 반환
+
+        # csv에 채워넣기
+        writer.writerow({'Game Name': line[1],'Google Play': app_size})
+        break
 
 # datafile.close()
